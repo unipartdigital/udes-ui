0d8209b55c9835a586e7b118b205a961318f19c8
richardwhitefoot <mouseholdonline@gmail.com>
2019-11-15 16:01:43 +0000

    Types added, package bump
    Signed-off-by: richardwhitefoot <mouseholdonline@gmail.com>

--- packages/udes-ui-form-dummy/webpack.config.js
+++ packages/udes-ui-form-dummy/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, "lib/"),
+    path: path.resolve(__dirname, "dist/"),
     library: libraryName,
     libraryTarget: "umd",
     umdNamedDefine: true,
