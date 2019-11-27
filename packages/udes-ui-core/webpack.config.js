45735769820fa7580e74028f367b61710aa159f7
Sean Quah <sean.quah@unipart.io>
2019-11-27 11:01:50 +0000

    Add udes-ui-core package skeleton
    Task: 374
    User-story: 336
    
    Signed-off-by: Sean Quah <sean.quah@unipart.io>

---
+++ packages/udes-ui-core/webpack.config.js
@@ -0,0 +1,24 @@
+const merge = require("webpack-merge");
+const common = require("../../webpack.config");
+const pkg = require("./package.json");
+const path = require("path");
+const libraryName = pkg.name;
+
+// Merge in local config here
+module.exports = merge(common, {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist/"),
+    library: libraryName,
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+  },
+});
