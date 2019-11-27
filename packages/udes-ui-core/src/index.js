1650ba451f3f8bd386f6bc49a301fd03674a23ca
Sean Quah <sean.quah@unipart.io>
2019-11-27 11:38:48 +0000

    Add Button component and unit tests
    Task: 375
    Task: 376
    User-story: 336
    
    Signed-off-by: Sean Quah <sean.quah@unipart.io>

--- packages/udes-ui-core/src/index.js
+++ packages/udes-ui-core/src/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
 export { Theme, ThemeOptions, theme, createModifiedTheme } from "./theme";
+export { default as Button } from "./button";
 export { default as ThemeProvider } from "./themeProvider";
 export { default as VerticalLayout } from "./verticalLayout";
