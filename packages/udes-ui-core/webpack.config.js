526a41c43ec46c78121dbdf68f40d7f125116e6b
Sean <48560964+squah-u@users.noreply.github.com>
2019-12-02 11:03:48 +0000

    Fix @udes-ui/core packaging issues (#22)
    * Fix webpack not emitting TypeScript declarations for index.js files
    
    Signed-off-by: Sean Quah <sean.quah@unipart.io>
    
    * Convert @udes-ui/core dependencies into peer dependencies
    
    Fixes multiple versions of @material-ui and styled-components being pulled
    in when the package is used as a dependency.
    Exclude styled-components and @material-ui from being bundled with webpack.
    Also move @types to devDependencies to avoid multiple versions of
    TypeScript declarations.
    
    Signed-off-by: Sean Quah <sean.quah@unipart.io>

--- packages/udes-ui-core/webpack.config.js
+++ packages/udes-ui-core/webpack.config.js
@@ -6,7 +6,7 @@ const libraryName = pkg.name;
 
 // Merge in local config here
 module.exports = merge(common, {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist/"),
@@ -19,6 +19,27 @@ module.exports = merge(common, {
     alias: {
       react: path.resolve(__dirname, "./node_modules/react"),
       "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+  },
+  externals: {
+    // Don't bundle @material-ui or styled-components
+    "@material-ui/core": {
+      commonjs: "@material-ui/core",
+      commonjs2: "@material-ui/core",
+      amd: "@material-ui/core",
+      root: "mui",
+    },
+    "@material-ui/styles": {
+      commonjs: "@material-ui/styles",
+      commonjs2: "@material-ui/styles",
+      amd: "@material-ui/styles",
+      root: "mui_styles",
+    },
+    "styled-components": {
+      commonjs: "styled-components",
+      commonjs2: "styled-components",
+      amd: "styled-components",
+      root: "styled",
     },
   },
 });
