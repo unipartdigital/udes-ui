ec844ca2645e4009c4928b76f8b135bc03dfa435
Robert Smith <robert.smith@unipart.io>
2019-12-06 16:44:26 +0000

    weirdFailStorybook

--- packages/udes-ui-storybook/storybook/stories/udes-ui-validation/stories.js
+++ packages/udes-ui-storybook/storybook/stories/udes-ui-validation/stories.js
@@ -1,12 +1,17 @@
 import React from "react";
 // eslint-disable-next-line max-len
-import Material from "../../../../udes-ui-validation/src/exampleComponent/materialUiComponent";
+import Material from "./exampleComponent/materialUiComponent";
+import { text } from "@storybook/addon-knobs";
 
 export default [
   {
     // eslint-disable-next-line react/display-name
     renderStory: () => {
-      return <Material />;
+      const name = text("Name", "dummy");
+      const label = text("Label", "Field Label");
+      const value = text("Value", "");
+
+      return <Material name={name} label={label} value={value} />;
     },
     storyName: "Basic",
     storyPath: "@udes-ui/validation|Validator",
