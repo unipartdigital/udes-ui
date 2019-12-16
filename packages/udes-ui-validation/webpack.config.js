e9e29c824e7d83ae2a0bb08d70ced384a865b737
richardwhitefoot <mouseholdonline@gmail.com>
2019-12-16 17:53:06 +0000

    fix: Validation webpack config for build
    Signed-off-by: richardwhitefoot <mouseholdonline@gmail.com>

--- packages/udes-ui-validation/webpack.config.js
+++ packages/udes-ui-validation/webpack.config.js
@@ -8,7 +8,7 @@ const libraryName = pkg.name;
 
 // Merge in local config here
 module.exports = merge(common, {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist/"),
